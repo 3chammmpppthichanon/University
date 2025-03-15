f5d66fb005ac82e7f45e9683333428a7bbd88144
3chammmpppthichanon <champ.thichanon@gmail.com>
2025-03-15 15:49:24 +0700

    Update

--- COS4101/accounts/models.py
+++ COS4101/accounts/models.py
@@ -1,3 +1,89 @@
 from django.db import models
-
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE_CHOICES = (
+        ('student', 'นักศึกษา'),
+        ('teacher', 'อาจารย์'),
+        ('admin', 'ผู้ดูแลระบบ'),
+    )
+    user_type = models.CharField(
+        max_length=10, 
+        choices=USER_TYPE_CHOICES,
+        default='student'
+    )
+    email = models.EmailField(blank=True,unique=True, default='')
+    name = models.CharField(max_length=255, default='', blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email
+    
+    def get_full_name(self):
+        return self.name
+    
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='student_profile'
+        )
+    student_id = models.CharField(
+        max_length=10, 
+        unique=True,
+        help_text='รหัสนักศึกษา 10 หลัก'
+        )
+    year = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(4)]
+    )
+
+class TeacherProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='teacher_profile'
+        )
+    expertise = models.CharField(
+        max_length=100,
+        help_text='ความเชี่ยวชาญ'
+        )
+    max_students = models.IntegerField(default=5)
